c28c951087c549b8fc683406a9cf39097c8b5657
philipp kern <philippk8@gmail.com>
2023-06-08 15:15:58 +0200

    node js server with sqlite database for highscores

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('highscores.db');
+
+// Create a highscores table if it doesn't exist
+db.serialize(() => {
+  db.run('CREATE TABLE IF NOT EXISTS highscores (id INTEGER PRIMARY KEY AUTOINCREMENT, playerName TEXT, score INTEGER)');
+});
+
+app.use(express.json());
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle high score submission
+app.post('/highscore', (req, res) => {
+  const highscore = req.body;
+
+  const query = 'INSERT INTO highscores (playerName, score) VALUES (?, ?)';
+  db.run(query, [highscore.playerName, highscore.score], (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error saving high score');
+    } else {
+      res.status(200).send('High score saved successfully');
+    }
+  });
+});
+
+// Retrieve highscores
+app.get('/highscores', (req, res) => {
+  const query = 'SELECT * FROM highscores ORDER BY score DESC';
+
+  db.all(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving high scores');
+    } else {
+      res.status(200).json(rows);
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
